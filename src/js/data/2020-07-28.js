ea68eacdf7518e0e6cd72cb8ab4df42b23822c64
Promesteiner <68930047+Promesteiner@users.noreply.github.com>
2020-07-28 19:23:34 -0600

    Create 2020-07-28.js

---
+++ src/js/data/2020-07-28.js
@@ -0,0 +1,563 @@
+dataSetVersion = "2020-07-28"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Setting",
+    key: "series",
+    tooltip: "Check this to restrict to certain series.",
+    checked: false,
+    sub: [
+      { name: "Agency", tooltip: "Starcross, Heavencross, and Fatecross", key: "SC" },
+      { name: "Black Widow", tooltip: "Stupid Occult Detective Bullshit", key: "BW" },
+      { name: "Suicide Kings", tooltip: "Local Tomboy Forced to Gamble Soul", key: "SK" },
+      { name: "Sweet Malus", tooltip: "Gods Go on Road Trip, Catastrophe Ensues", key: "SM" },
+      { name: "Roleplays and etc.", tooltip: "Characters made for roleplays or for some other person's setting", key: "RP" },     
+      { name: "Rafflesia", tooltip: "Rafflesia", key: "RN" }     
+    ]
+  },
+{
+  name: 'Filter Losers Not on the Wiki',
+  key: 'notwiki',
+  tooltip: 'Check this to get the characters not on the wiki yet out of there.'
+  checked: true,
+}  
+];
+
+dataSet[dataSetVersion].characterData = [
+   {
+    name: "Minna Yamane",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "EQUINOX",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Kyarne",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Anya Kimura",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Enkh Khenbishiin",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Milky Way",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Mariposa",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  
+  {
+    name: "Alice Kang (SOLSTICE)",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Venus Kang",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Neptune",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },  
+  {
+    name: "Sabrina Trenchard (Ceres)",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Instant",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Kalaziel (Terror)",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Warden",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Langosta",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Deleter",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Hanael (Observer)",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  
+  {
+    name: "Harahel",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Raziel",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Canace",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Baphomet",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Belphegor",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Metatron",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Lucifer",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+
+  {
+    name: "Hexenn",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Lamiet",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+
+  
+  {
+    name: "Arista",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Morsitans",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Murmur",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Beelzebub",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+
+  {
+    name: "Azrael",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },  
+  
+  {
+    name: "Mastema",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Abaddon",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Uriel",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Sariel",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Jophiel",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Gabriel",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Raphael",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Yaldabaoth",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+
+  {
+    name: "Sophia",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Synthesizer Logos",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Logos Prime",
+    img: "",
+    opts: {
+      series: [ "SC" ],
+      notwiki: false
+    }
+  },
+
+  {
+    name: "Iris Widow",
+    img: "",
+    opts: {
+      series: [ "BW" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Sherry Watson",
+    img: "",
+    opts: {
+      series: [ "BW" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Ivy Branwen",
+    img: "",
+    opts: {
+      series: [ "BW" ],
+      notwiki: true
+    }
+  },
+
+  {
+    name: "Annis Doyle",
+    img: "",
+    opts: {
+      series: [ "BW" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Blake Corbet",
+    img: "",
+    opts: {
+      series: [ "BW" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Hanami Yako",
+    img: "",
+    opts: {
+      series: [ "BW" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Trespasser",
+    img: "",
+    opts: {
+      series: [ "BW" ],
+      notwiki: false
+    }
+  },
+  
+  {
+    name: "Roxanne Royale",
+    img: "",
+    opts: {
+      series: [ "SK" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Rachel Reiss",
+    img: "",
+    opts: {
+      series: [ "SK" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Jack Leblanc",
+    img: "",
+    opts: {
+      series: [ "SK" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Noelle Germaine",
+    img: "",
+    opts: {
+      series: [ "SK" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Giselle Glamour",
+    img: "",
+    opts: {
+      series: [ "SK" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "The Ace of Spades",
+    img: "",
+    opts: {
+      series: [ "SK" ],
+      notwiki: true
+    }
+  },
+  
+  {
+    name: "Nemesis",
+    img: "",
+    opts: {
+      series: [ "SM" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Nemesis Cain",
+    img: "",
+    opts: {
+      series: [ "SM" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Eris Strife",
+    img: "",
+    opts: {
+      series: [ "SM" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Momo",
+    img: "",
+    opts: {
+      series: [ "SM" ],
+      notwiki: true
+    }
+  },
+
+  {
+    name: "Merche Ramón",
+    img: "",
+    opts: {
+      series: [ "RP" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Anastasia",
+    img: "",
+    opts: {
+      series: [ "RP" ],
+      notwiki: true
+    }
+  },
+  {
+    name: "Sidney Lynch",
+    img: "",
+    opts: {
+      series: [ "RP" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Mia Lynch",
+    img: "",
+    opts: {
+      series: [ "RP" ],
+      notwiki: false
+    }
+  },
+  {
+    name: "Lili Király",
+    img: "",
+    opts: {
+      series: [ "RP" ],
+      notwiki: false
+    }
+  },
+
+  
+  {
+    name: "Rafflesia Nightingale",
+    img: "",
+    opts: {
+      series: [ "RN" ],
+      notwiki: true
+    }
+  }  
+];
