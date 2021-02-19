70907196e8749fd3d13f4446c2337597814ed49e
Promesteiner <68930047+Promesteiner@users.noreply.github.com>
2021-02-18 18:11:28 -0700

    Update 2020-07-28.js

--- src/js/data/2020-07-28.js
+++ src/js/data/2020-07-28.js
@@ -36,7 +36,7 @@ dataSet[dataSetVersion].options = [
   checked: false
   },
  {
-  name: 'Filter Miscellaneous',
+  name: 'Filter Misc.',
   key: 'nb',
   tooltip: 'Check this to get the miscellaneously gendered characters out of there.',
   checked: false
